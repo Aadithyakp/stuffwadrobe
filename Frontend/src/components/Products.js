0a7d0ee7c9521dfe4563f1789e96744250d269f6
Aadithyakp <71168518+Aadithyakp@users.noreply.github.com>
2025-03-21 14:16:50 -0400

    Update Products.js

--- Frontend/src/components/Products.js
+++ Frontend/src/components/Products.js
@@ -17,7 +17,7 @@ const Products = ({cat,filters,sort}) => {
    useEffect(()=>{
     const getProducts = async () => {
       try {
-        const endpoint = (cat?`https://stuffwadrobe.onrender.com/products/?category=${cat}`:"https://stuffwadrobe.onrender.com/api/products/");
+        const endpoint = (cat?`https://stuffwadrobe.onrender.com/api/products/?category=${cat}`:"https://stuffwadrobe.onrender.com/api/products/");
         const res = await axios.get(endpoint);
         
       setProducts(res.data);
